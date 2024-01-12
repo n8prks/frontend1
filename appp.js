2baa078e4211aa7645d52b92d84cb26f2b6d6578
n8 <nthnprks@gmail.com>
2024-01-12 10:50:29 -0800

    First code dump from local repo

---
+++ appp.js
@@ -0,0 +1,14 @@
+function handleCreatePost(){
+    document.getElementById("post-form-container").style.display = "block"
+}
+function hidePostForm(){
+    document.getElementById("post-form-container").style.display = "none"
+
+}
+
+
+
+
+/* this is the code for the first website that deals with divs and
+        flex.grow.  I used a pic of a sunset for the image.  This excercise
+        focuses on website layout. */
